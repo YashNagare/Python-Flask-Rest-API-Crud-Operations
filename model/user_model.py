47505d1fd98a4407e2dac6a63e9616034cee9916
YashNagare <yashpnagare@gmail.com>
2024-03-23 19:14:45 +0530

    created CRUD operations

--- model/user_model.py
+++ model/user_model.py
@@ -6,6 +6,7 @@ class user_model:
     def __init__(self):
         try:
             self.con = mysql.connector.connect(host="localhost", user="root", password="root", database="flask_tutorial")
+            self.con.autocommit = True
             self.cur = self.con.cursor(dictionary=True)
             print("Connection successful")
         except:
@@ -19,3 +20,21 @@ class user_model:
             return json.dumps(result)
         else:
             return "No data found"
+
+    def user_addone_model(self, data):
+        self.cur.execute(f"INSERT INTO users(name, email, phone, role, password) VALUES('{data['name']}', '{data['email']}', '{data['phone']}', '{data['role']}', '{data['password']}')")
+        return "User created successfully"
+
+    def user_update_model(self, data):
+        self.cur.execute(f"UPDATE users SET name='{data['name']}', email='{data['email']}', phone='{data['phone']}', role='{data['role']}', password='{data['password']}' WHERE id='{data['id']}'")
+        if self.cur.rowcount > 0:
+            return "User updated successfully"
+        else:
+            return "Nothing to update"
+
+    def user_delete_model(self, id):
+        self.cur.execute(f"DELETE FROM users WHERE id={id}")
+        if self.cur.rowcount > 0:
+            return "User deleted successfully"
+        else:
+            return "Nothing to delete"
